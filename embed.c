6e723c9b687b23dceb519b017d951f8311bc40ab
BeratDizdar <139409706+BeratDizdar@users.noreply.github.com>
2026-07-09 09:26:57 +0300

    son formu: .\embed.exe .\t.png sonic.h sonic_data

--- embed.c
+++ embed.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-main(c) {
-    FILE*i=fopen("test.txt","rb"),*o=fopen("a.h","w");
-    for(fputs("unsigned char a[]={",o);~(c=fgetc(i));)
-    fprintf(o,"%d,",c);fputs("};",o);}
+main(c,v)char**v;{
+    FILE*i=fopen(v[1],"rb"),*o=fopen(v[2],"w");
+    for(fprintf(o,"unsigned char %s[]={",v[3]);
+    ~(c=fgetc(i));)fprintf(o,"%d,",c);fputs("};",o);}
